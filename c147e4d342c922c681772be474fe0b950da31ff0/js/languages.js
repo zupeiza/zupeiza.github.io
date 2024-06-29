3a1fe8216c00cfe75235315bbc6b5f45bfad04be
zupeiza <zupeiza@gmail.com>
2024-06-29 18:43:08 +0200

    Correction languages.js

--- c147e4d342c922c681772be474fe0b950da31ff0/js/languages.js
+++ c147e4d342c922c681772be474fe0b950da31ff0/js/languages.js
@@ -12,7 +12,7 @@ function setLanguagePreference(lang) {
 }
 // Function to fetch language data
 async function fetchLanguageData(lang) {
-    const response = await fetch('../languages/${lang}.json');
+    const response = await fetch(`../languages/${lang}.json`);
     return response.json();
 }
 // Function to change language
@@ -25,7 +25,7 @@ async function changeLanguage(lang) {
 
 // Call updateContent() on page load
 window.addEventListener('DOMContentLoaded', async () => {
-    const userPreferredLanguage = localStorage.getItem('language') || 'en';
+    const userPreferredLanguage = localStorage.getItem('language') || 'es';
     const langData = await fetchLanguageData(userPreferredLanguage);
     updateContent(langData);
     toggleArabicStylesheet(userPreferredLanguage);
